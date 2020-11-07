c3f02b4504b012141fbd1ee7b0d1b2720f34e2cc
Amey Sunu <ameysunu00719@gmail.com>
2020-11-07 12:23:57 +0300

    google sign in added

--- lib/login.dart
+++ lib/login.dart
@@ -1,5 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:hackpsu/homewidget.dart';
 import 'package:hexcolor/hexcolor.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+String name;
+String email;
+String imageUrl;
+
+final FirebaseAuth _auth = FirebaseAuth.instance;
+final GoogleSignIn googleSignIn = GoogleSignIn();
+
+Future<String> signInWithGoogle() async {
+  await Firebase.initializeApp();
+
+  final GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
+  final GoogleSignInAuthentication googleSignInAuthentication =
+      await googleSignInAccount.authentication;
+
+  final AuthCredential credential = GoogleAuthProvider.credential(
+    accessToken: googleSignInAuthentication.accessToken,
+    idToken: googleSignInAuthentication.idToken,
+  );
+
+  final UserCredential authResult =
+      await _auth.signInWithCredential(credential);
+  final User user = authResult.user;
+
+  if (user != null) {
+    assert(user.email != null);
+    assert(user.displayName != null);
+    assert(user.photoURL != null);
+
+    // Store the retrieved data
+    name = user.displayName;
+    email = user.email;
+    imageUrl = user.photoURL;
+
+    // Only taking the first part of the name, i.e., First Name
+    if (name.contains(" ")) {
+      name = name.substring(0, name.indexOf(" "));
+    }
+
+    assert(!user.isAnonymous);
+    assert(await user.getIdToken() != null);
+
+    final User currentUser = _auth.currentUser;
+    assert(user.uid == currentUser.uid);
+
+    print('signInWithGoogle succeeded: $user');
+
+    return '$user';
+  }
+
+  return null;
+}
+
+Future<void> signOutGoogle() async {
+  await googleSignIn.signOut();
+
+  print("User Signed Out");
+}
 
 class Login extends StatefulWidget {
   @override
@@ -14,7 +76,8 @@ class _LoginState extends State<Login> {
         backgroundColor: HexColor('#EBBF6A'),
         title: Text(
           'Login',
-          style: TextStyle(fontFamily: 'Roboto Medium', color: Colors.black),
+          style: TextStyle(
+              fontFamily: 'Roboto Medium', color: HexColor('#000000')),
         ),
         elevation: 0,
       ),
@@ -64,17 +127,17 @@ class _LoginState extends State<Login> {
                       borderRadius: BorderRadius.circular(10.0),
                     ),
                     onPressed: () {
-                      // signInWithGoogle().then((result) {
-                      //   if (result != null) {
-                      //     Navigator.of(context).push(
-                      //       MaterialPageRoute(
-                      //         builder: (context) {
-                      //           return Home();
-                      //         },
-                      //       ),
-                      //     );
-                      //   }
-                      // });
+                      signInWithGoogle().then((result) {
+                        if (result != null) {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (context) {
+                                return HomeWidget();
+                              },
+                            ),
+                          );
+                        }
+                      });
                     },
                   ),
                 ),
