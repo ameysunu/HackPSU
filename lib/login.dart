3b7821a617274ed532b5deb3b8a91def51212ce6
Amey Sunu <ameysunu00719@gmail.com>
2020-11-07 11:54:54 +0300

    ui updated

--- lib/login.dart
+++ lib/login.dart
@@ -10,57 +10,76 @@ class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: HexColor('#EBBF6A'),
+        title: Text(
+          'Login',
+          style: TextStyle(fontFamily: 'Roboto Medium', color: Colors.black),
+        ),
+        elevation: 0,
+      ),
       backgroundColor: HexColor('#EBBF6A'),
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: RaisedButton(
-                  color: HexColor('#9296F0'),
-                  child: Container(
-                    height: 50,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(10.0),
-                          child: Container(
-                            height: 25,
-                            width: 25,
-                            child: Image.asset("images/google.png"),
-                          ),
-                        ),
-                        Text(
-                          "Sign in with Google",
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontFamily: "Roboto Medium",
-                              fontSize: 18),
-                        ),
-                      ],
-                    ),
-                  ),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  onPressed: () {
-                    // signInWithGoogle().then((result) {
-                    //   if (result != null) {
-                    //     Navigator.of(context).push(
-                    //       MaterialPageRoute(
-                    //         builder: (context) {
-                    //           return Home();
-                    //         },
-                    //       ),
-                    //     );
-                    //   }
-                    // });
-                  },
+      body: DecoratedBox(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("images/home.png"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: SafeArea(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.6,
                 ),
-              ),
-            ],
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: RaisedButton(
+                    color: HexColor('#9296F0'),
+                    child: Container(
+                      height: 50,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(10.0),
+                            child: Container(
+                              height: 25,
+                              width: 25,
+                              child: Image.asset("images/google.png"),
+                            ),
+                          ),
+                          Text(
+                            "Sign in with Google",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontFamily: "Roboto Medium",
+                                fontSize: 18),
+                          ),
+                        ],
+                      ),
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    onPressed: () {
+                      // signInWithGoogle().then((result) {
+                      //   if (result != null) {
+                      //     Navigator.of(context).push(
+                      //       MaterialPageRoute(
+                      //         builder: (context) {
+                      //           return Home();
+                      //         },
+                      //       ),
+                      //     );
+                      //   }
+                      // });
+                    },
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
