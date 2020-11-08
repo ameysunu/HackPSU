0b6e8f741215247ad211c8fad8ee2df1212a651a
Amey Sunu <ameysunu00719@gmail.com>
2020-11-08 13:19:28 +0300

    ui fixes

--- lib/pages/result.dart
+++ lib/pages/result.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:hackpsu/homewidget.dart';
 import 'package:hackpsu/quiz.dart';
 
 class Result extends StatelessWidget {
@@ -8,28 +9,46 @@ class Result extends StatelessWidget {
   Result(this.tscore);
 
   String get results {
-    var resultPhrase =
-        "I can not be more impressed and grateful with the level of energy, communication,  professionalism, friendliness, expertise and skill .";
-    if (tscore <= 30)
-      resultPhrase += " Total Score : $tscore \n\n Could have done better!!";
-    else if (tscore == 40)
+    var resultPhrase = "Great work, trying to attempt the quiz";
+    if (tscore < 30) {
       resultPhrase +=
-          " Total Score : $tscore \n\n MANAGEABLE, I expected a perfect score";
-    else
+          "\nTotal Score: $tscore\n\n But you are'nt still clear with basics. Go back and complete the tutorial once again";
+    } else if (tscore == 30) {
       resultPhrase +=
-          " Total Score : $tscore \n\n I knew it.. You are awesome:)";
+          "\nTotal Score: $tscore\n\n Great work! What are you waiting for? Get your credit card already";
+    }
     return resultPhrase;
   }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.all(10),
-      child: Text(
-        results,
-        style: TextStyle(
-            fontSize: 20, fontWeight: FontWeight.bold, fontFamily: 'Poppins'),
-        textAlign: TextAlign.center,
+      decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('images/code.png'), fit: BoxFit.cover)),
+      child: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.all(10),
+            child: Text(
+              results,
+              style: TextStyle(fontSize: 20, fontFamily: 'SourceSansPro'),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.refresh,
+              color: Colors.white,
+            ),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => HomeWidget()),
+              );
+            },
+          )
+        ],
       ),
     );
   }
